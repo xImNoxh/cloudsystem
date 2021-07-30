a2c6fcd80325a99ff9eafd1a3f138e868e36e7ab
HttxMarco <mircolindenau04@gmail.com>
2021-07-30 17:51:22 +0200

    recode listener of signsystem

--- polocloud-addons/polocloud-addons-signs/src/main/java/de/polocloud/signs/collectives/CollectiveSpigotListener.java
+++ polocloud-addons/polocloud-addons-signs/src/main/java/de/polocloud/signs/collectives/CollectiveSpigotListener.java
@@ -2,13 +2,13 @@ package de.polocloud.signs.collectives;
 
 import de.polocloud.api.gameserver.IGameServer;
 import de.polocloud.plugin.api.CloudExecutor;
-import de.polocloud.plugin.api.spigot.event.CloudPlayerSwitchServerEvent;
-import de.polocloud.plugin.api.spigot.event.CloudServerStartedEvent;
-import de.polocloud.plugin.api.spigot.event.CloudServerStoppedEvent;
-import de.polocloud.plugin.api.spigot.event.CloudServerUpdatedEvent;
+import de.polocloud.plugin.api.spigot.event.*;
 import de.polocloud.signs.SignService;
 import de.polocloud.signs.bootstrap.SignBootstrap;
+import de.polocloud.signs.config.SignConfig;
+import de.polocloud.signs.executes.ExecuteService;
 import de.polocloud.signs.signs.IGameServerSign;
+import de.polocloud.signs.signs.cache.IGameServerSignCache;
 import de.polocloud.signs.utils.PlayerUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -20,40 +20,52 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CollectiveSpigotListener implements Listener {
 
     private SignService signService;
+    private SignConfig signConfig;
+    private IGameServerSignCache cache;
+    private ExecuteService executeService;
 
     public CollectiveSpigotListener() {
-        signService = SignService.getInstance();
+
+        this.signService = SignService.getInstance();
+        this.signConfig = signService.getSignConfig();
+        this.cache = signService.getCache();
+        this.executeService = signService.getExecuteService();
+
         Bukkit.getPluginManager().registerEvents(this, SignBootstrap.getInstance());
     }
 
     @EventHandler
     public void handle(PlayerInteractEvent event) {
-        if(!(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_BLOCK))) return;
+        if (!(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.LEFT_CLICK_BLOCK)))
+            return;
         if (event.getClickedBlock() == null || !event.getClickedBlock().getType().equals(Material.WALL_SIGN)) return;
-
         Sign sign = (Sign) event.getClickedBlock().getState();
-        IGameServerSign gameSign = signService.getCache().stream().filter(s -> s.getSign().equals(sign)).findAny().orElse(null);
-        if (gameSign == null || gameSign.getGameServer() == null) return;
-
-
+        IGameServerSign gameSign = cache.stream().filter(s -> s.getSign().equals(sign)).findAny().orElse(null);
         Player player = event.getPlayer();
+        if (gameSign.getTemplate().isMaintenance()) {
+            player.sendMessage(signConfig.getSignMessages().getMaintenanceConnected());
+            return;
+        }
+        if (gameSign == null || gameSign.getGameServer() == null) return;
         CloudExecutor.getInstance().getCloudPlayerManager().getOnlinePlayer(player.getUniqueId()).thenAccept(key -> {
             if (key.getMinecraftServer().getSnowflake() == gameSign.getGameServer().getSnowflake()) {
                 player.sendMessage(signService.getSignConfig().getSignMessages().getAlreadySameService());
                 return;
             }
-
-            if (!signService.getSignConfig().isConnectIfFull()) {
-                if (signService.getSignConfig().isCanUseConnectIfFullPermission()) {
-                    if (player.hasPermission(signService.getSignConfig().getConnectIfFullPermission())) {
+            if (!signConfig.isConnectIfFull()) {
+                if (signConfig.isCanUseConnectIfFullPermission()) {
+                    if (player.hasPermission(signConfig.getConnectIfFullPermission())) {
                         PlayerUtils.sendService(gameSign.getGameServer(), player);
                         return;
                     }
                 }
-                player.sendMessage(signService.getSignConfig().getSignMessages().getConnectedIfServiceIsFull());
+                player.sendMessage(signConfig.getSignMessages().getConnectedIfServiceIsFull());
                 return;
             }
             PlayerUtils.sendService(gameSign.getGameServer(), player);
@@ -63,7 +75,7 @@ public class CollectiveSpigotListener implements Listener {
     @EventHandler
     public void handle(SignChangeEvent event) {
         Sign sign = (Sign) event.getBlock().getState();
-        IGameServerSign gameSign = signService.getCache().stream().filter(s -> s.getLocation().equals(sign.getLocation())).findAny().get();
+        IGameServerSign gameSign = executeService.getServiceInspectExecute().getGameSignBySign(sign);
         if (gameSign == null) return;
         gameSign.reloadSign(sign);
         gameSign.updateSign();
@@ -72,47 +84,35 @@ public class CollectiveSpigotListener implements Listener {
 
     @EventHandler
     public void handle(CloudServerStartedEvent event) {
-        IGameServerSign sign = signService.getFreeTemplateSign(event.getGameServer());
-        sign.setGameServer(event.getGameServer());
-        sign.displayService();
+        executeService.getServiceUpdateExecute().update(executeService.getServiceInspectExecute().getFreeTemplateSign(event.getGameServer()), event.getGameServer());
     }
 
     @EventHandler
     public void handle(CloudPlayerSwitchServerEvent event) {
-        event.getTo().thenAccept(service -> {
-            IGameServerSign gameServerSign = signService.getCache().stream().filter(key ->
-                key.getGameServer().getSnowflake() == service.getSnowflake()).findAny().orElse(null);
-            if (gameServerSign == null) return;
-            gameServerSign.setGameServer(service);
-            gameServerSign.displayService();
-        });
-
-        event.getFrom().thenAccept(service -> {
-            IGameServerSign gameServerSign = signService.getCache().stream().filter(key ->
-                key.getGameServer().getSnowflake() == service.getSnowflake()).findAny().orElse(null);
-            if (gameServerSign == null) return;
-            gameServerSign.setGameServer(service);
-            gameServerSign.displayService();
-        });
+        event.getTo().thenAccept(service -> executeService.getServiceUpdateExecute().update( executeService.getServiceInspectExecute().execute(service), service));
+        event.getFrom().thenAccept(service -> executeService.getServiceUpdateExecute().update( executeService.getServiceInspectExecute().execute(service), service));
     }
 
     @EventHandler
     public void handle(CloudServerUpdatedEvent event) {
-        IGameServerSign gameServerSign = signService.getCache().stream().filter(key -> key.getGameServer() != null && key.getGameServer().getSnowflake() == event.getGameServer().getSnowflake()).findAny().orElse(null);
-        if (gameServerSign == null) return;
-        gameServerSign.setGameServer(event.getGameServer());
-        gameServerSign.displayService();
+        executeService.getServiceUpdateExecute().update(executeService.getServiceInspectExecute().execute(event.getGameServer()), event.getGameServer());
+    }
+
+    @EventHandler
+    public void handle(TemplateMaintenanceUpdateEvent event) {
+        List<IGameServerSign> gameServerSign = cache.stream().filter(key -> key.getTemplate().getName().equals(event.getTemplate().getName())).collect(Collectors.toList());
+        for (IGameServerSign sign : gameServerSign) {
+            sign.setTemplate(event.getTemplate());
+            sign.writeSign();
+        }
     }
 
     @EventHandler
     public void handle(CloudServerStoppedEvent event) {
-        IGameServer gameServer = event.getGameServer();
-        IGameServerSign gameServerSign = SignService.getInstance().getCache().stream().filter(key ->
-            key.getGameServer().getSnowflake() == gameServer.getSnowflake()).findAny().orElse(null);
+        IGameServerSign gameServerSign = executeService.getServiceInspectExecute().execute(event.getGameServer());
         if (gameServerSign == null) return;
         gameServerSign.setGameServer(null);
         gameServerSign.writeSign();
-
     }
 
 }
