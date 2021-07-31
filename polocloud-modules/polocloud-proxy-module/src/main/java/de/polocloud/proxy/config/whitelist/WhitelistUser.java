777c64c257640e7e41c183f49054b8150f25b6ab
HttxMarco <mircolindenau04@gmail.com>
2021-07-31 19:39:43 +0200

    add whitelist at proxy module

---
+++ polocloud-modules/polocloud-proxy-module/src/main/java/de/polocloud/proxy/config/whitelist/WhitelistUser.java
@@ -0,0 +1,30 @@
+package de.polocloud.proxy.config.whitelist;
+
+import java.util.UUID;
+
+public class WhitelistUser {
+
+    private UUID uuid;
+    private String name;
+
+    public WhitelistUser(UUID uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
