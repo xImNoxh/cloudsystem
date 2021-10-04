7257728b8e97ae40172480114491c1889fa9623a
DasLixou <82600264+DasLixou@users.noreply.github.com>
2021-10-04 21:24:51 +0200

    Added basic VersionInstaller

--- polocloud-wrapper/src/main/java/de/polocloud/wrapper/version/VersionInstaller.java
+++ polocloud-wrapper/src/main/java/de/polocloud/wrapper/version/VersionInstaller.java
@@ -1,9 +1,15 @@
 package de.polocloud.wrapper.version;
 
+import de.polocloud.api.config.FileConstants;
 import de.polocloud.api.console.ConsoleColors;
 import de.polocloud.api.gameserver.helper.GameServerVersion;
 import de.polocloud.api.logger.PoloLogger;
 import de.polocloud.api.logger.helper.LogLevel;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 
 /**
  * This class was developed by DasLixou :3
@@ -13,17 +19,19 @@ public class VersionInstaller {
 
     public static void installVersion(GameServerVersion version) {
         PoloLogger.print(LogLevel.INFO, "§7Downloading §bVersion §e" + version.getTitle() + "§7...");
-        /*serverFile.getParentFile().mkdirs();
-        try {
-            FileUtils.copyURLToFile(new URL(template.getVersion().getUrl()), serverFile);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }*/
+        // TODO: NEED HERE TO DOWNLOAD THE JAR AND PUT IT INTO PATCH REGION
         PoloLogger.print(LogLevel.INFO, "§7Download of §bVersion §e" + version.getTitle() + ConsoleColors.GREEN + " successfully " + ConsoleColors.GRAY + "completed.");
         if(version.isPatchable()) {
             PoloLogger.print(LogLevel.INFO, "§7Patching §bVersion §e" + version.getTitle() + "§7...");
 
             PoloLogger.print(LogLevel.INFO, "§7Patch of §bVersion §e" + version.getTitle() + ConsoleColors.GREEN + " successfully " + ConsoleColors.GRAY + "completed.");
+        }
+        File serverFile = new File(FileConstants.WRAPPER_STORAGE_VERSIONS, version.getTitle() + ".jar");
+        serverFile.getParentFile().mkdirs();
+        try {
+            FileUtils.copyURLToFile(new URL(version.getUrl()), serverFile);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
