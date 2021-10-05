3b387cddc76480f71fe335c3667ab6471b6fb689
DasLixou <82600264+DasLixou@users.noreply.github.com>
2021-10-05 14:48:37 +0200

    Added Patch Folder to File Constants.
    Fixed Deleting Temp Folder in Bootstrap-checkAndDeleteTmpFolder().
    
    Added checkAndDeletePatchFolder() to Bootstrap

--- polocloud-wrapper/src/main/java/de/polocloud/wrapper/version/VersionInstaller.java
+++ polocloud-wrapper/src/main/java/de/polocloud/wrapper/version/VersionInstaller.java
@@ -5,6 +5,7 @@ import de.polocloud.api.console.ConsoleColors;
 import de.polocloud.api.gameserver.helper.GameServerVersion;
 import de.polocloud.api.logger.PoloLogger;
 import de.polocloud.api.logger.helper.LogLevel;
+import de.polocloud.api.util.PoloHelper;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
@@ -18,14 +19,24 @@ import java.net.URL;
 public class VersionInstaller {
 
     public static void installVersion(GameServerVersion version) {
-        PoloLogger.print(LogLevel.INFO, "§7Downloading §bVersion §e" + version.getTitle() + "§7...");
-        // TODO: NEED HERE TO DOWNLOAD THE JAR AND PUT IT INTO PATCH REGION
-        PoloLogger.print(LogLevel.INFO, "§7Download of §bVersion §e" + version.getTitle() + ConsoleColors.GREEN + " successfully " + ConsoleColors.GRAY + "completed.");
-        if(version.isPatchable()) {
-            PoloLogger.print(LogLevel.INFO, "§7Patching §bVersion §e" + version.getTitle() + "§7...");
+        PoloLogger.print(LogLevel.INFO, "§7Downloading §b" + version.getTemplateType().getDisplayName() + "-Version§7... (§3" + version.getTitle() + "§7)");
 
-            PoloLogger.print(LogLevel.INFO, "§7Patch of §bVersion §e" + version.getTitle() + ConsoleColors.GREEN + " successfully " + ConsoleColors.GRAY + "completed.");
+        // TODO: NEED HERE TO DOWNLOAD THE JAR AND PUT IT INTO PATCH REGION
+        File patchServer = new File(FileConstants.WRAPPER_PATCHER_FOLDER, version.getTitle());
+        patchServer.mkdirs();
+        try {
+            FileUtils.copyURLToFile(new URL(version.getUrl()), new File(patchServer, version.getTitle() + ".jar"));
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        PoloLogger.print(LogLevel.INFO, "Downloading of (§3" + version.getTitle() + "§7)" + ConsoleColors.GREEN + " successfully " + ConsoleColors.GRAY + "completed.");
+        if(version.isPatchable()) {
+            PoloLogger.print(LogLevel.INFO, "§7Patching §b" + version.getTemplateType().getDisplayName() + "-Version§7... (§3" + version.getTitle() + "§7)");
+
+            PoloLogger.print(LogLevel.INFO, "Patch of (§3" + version.getTitle() + "§7)" + ConsoleColors.GREEN + " successfully " + ConsoleColors.GRAY + "completed.");
+        }
+
         File serverFile = new File(FileConstants.WRAPPER_STORAGE_VERSIONS, version.getTitle() + ".jar");
         serverFile.getParentFile().mkdirs();
         try {
@@ -33,6 +44,8 @@ public class VersionInstaller {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        PoloHelper.deleteFolder(patchServer);
     }
 
 }
